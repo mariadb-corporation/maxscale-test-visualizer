8a5ef691651e5d1b149afb82f2e540715151fa11
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2018-02-13 23:53:24 +0300

    Remove scripts for uploading testruns to ReportPortal (refs #23730)

--- report-portal-scripts/lib/config.rb
+++ report-portal-scripts/lib/config.rb
@@ -1,7 +1,6 @@
 # Class for load configuration from YAML-config file
 class Config
   attr_reader :user, :password, :db_name, :project_name, :auth_token,
-              :test_run_count, :repository_url, :logs_dir_url,
               :report_portal_url
 
   require 'yaml'
@@ -21,11 +20,8 @@ class Config
     @user = database_root['user']
     @password = database_root['password']
     @db_name = database_root['db_name']
-    @test_run_count = database_root['test_run_count_to_import']
     @project_name = report_portal_root['project_name']
     @auth_token = report_portal_root['auth_token']
-    @repository_url = report_portal_root['repository_url']
-    @logs_dir_url = report_portal_root['logs_dir_url']
     @report_portal_url = report_portal_root['url']
   end
 
@@ -43,9 +39,9 @@ class Config
 
     if database.nil? || rp.nil? ||
        database['db_name'].nil? || database['user'].nil? ||
-       database['password'].nil? || database['test_run_count_to_import'].nil? ||
+       database['password'].nil? ||
        rp['project_name'].nil? || rp['auth_token'].nil? ||
-       rp['repository_url'].nil? || rp['logs_dir_url'].nil? || rp['url'].nil?
+       rp['url'].nil?
 
       puts "Error: incorrect config.yml file\n"
       print_config_example
@@ -63,14 +59,11 @@ database:
     db_name: <db_name>
     user: <user_name>
     password: <password>
-    test_run_count_to_import: 20
 
 report_portal:
     url: http://localhost:8080
     project_name: <project_name>
     auth_token: "8ee428b5-dcc5-4fba-8e4f-462863dbba15" # Get it from http://localhost:8080/ui/?#user-profile
-    repository_url: https://github.com/mariadb-corporation/MaxScale
-    logs_dir_url: http://max-tst-01.mariadb.com/LOGS/
 ------------
     EOF
   end
