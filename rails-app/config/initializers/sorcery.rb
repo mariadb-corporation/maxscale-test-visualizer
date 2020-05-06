787fc64723dc75036f2dec7869ca44be020384c7
Evgeny Vlasov <evgeny.vlasov@fruct.org>
2020-05-06 21:54:32 +0300

    Add google auhorization (refs #26945)

--- rails-app/config/initializers/sorcery.rb
+++ rails-app/config/initializers/sorcery.rb
@@ -80,7 +80,7 @@ Rails.application.config.sorcery.configure do |config|
   # i.e. [:twitter, :facebook, :github, :linkedin, :xing, :google, :liveid, :salesforce, :slack, :line].
   # Default: `[]`
   #
-  config.external_providers = [:github]
+  config.external_providers = [:google]
 
   # You can change it by your local ca_file. i.e. '/etc/pki/tls/certs/ca-bundle.crt'
   # Path to ca_file. By default use a internal ca-bundle.crt.
@@ -128,15 +128,15 @@ Rails.application.config.sorcery.configure do |config|
   # config.instagram.user_info_mapping = {:email => "username"}
   # config.instagram.access_permissions = ["basic", "public_content", "follower_list", "comments", "relationships", "likes"]
   #
-  config.github.key = ENV['github_app_id']
-  config.github.secret = ENV['github_app_secret']
-  config.github.callback_url = ENV['github_sorcery_callback_url']
-  config.github.user_info_mapping = {
-      :nickname => "login",
-      :name => "name",
-      :image => "avatar_url"
-  }
-  config.github.scope = ""
+  #config.github.key = ENV['github_app_id']
+  #config.github.secret = ENV['github_app_secret']
+  #config.github.callback_url = ENV['github_sorcery_callback_url']
+  #config.github.user_info_mapping = {
+  #    :nickname => "login",
+  #    :name => "name",
+  #    :image => "avatar_url"
+  #}
+  #config.github.scope = ""
   #
   # config.paypal.key = ""
   # config.paypal.secret = ""
@@ -154,11 +154,11 @@ Rails.application.config.sorcery.configure do |config|
   # config.auth0.callback_url = "https://0.0.0.0:3000/oauth/callback?provider=auth0"
   # config.auth0.site = "https://example.auth0.com"
   #
-  # config.google.key = ""
-  # config.google.secret = ""
-  # config.google.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=google"
-  # config.google.user_info_mapping = {:email => "email", :username => "name"}
-  # config.google.scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
+  config.google.key = ENV['google_app_id']
+  config.google.secret = ENV['google_app_secret']
+  config.google.callback_url = ENV['google_sorcery_callback_url']
+  config.google.user_info_mapping = {:nickname => "email", :name => "name", :image => "picture"}
+  config.google.scope = "https://www.googleapis.com/auth/userinfo.email https://www.googleapis.com/auth/userinfo.profile"
   #
   # For Microsoft Graph, the key will be your App ID, and the secret will be your app password/public key.
   # The callback URL "can't contain a query string or invalid special characters"
